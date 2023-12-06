22b2e7ba6bf8fd67d0e218508f1fdef74dfe163e
ColourBird <130944207+ColourBird@users.noreply.github.com>
2023-12-06 11:24:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
-{ 
- "users":[
-            {
-             "name":"ユーザー1",
-             "pass":"パスワード1",
-             "favorites": {
-                "pets":["タマ", "ポチ","ミケ"],
-                "people": [
-                  { "family": "佐藤", "last": "太郎" },
-                  { "family": "鈴木", "last": "花子" }
-                ],
-                "cars": [
-                  { "company": "トヨタ", "car": "カローラ" },
-                  { "company": "日産", "car": "ノート" }
-                ]},
-             "history":[
-                { "type": "pets", "name": "タマ", "concept": "ペット名" },
-                { "type": "people", "name": "佐藤太郎", "concept": "日本人の苗字と名前" },
-                { "type": "cars", "name": "トヨタカローラ", "concept": "車のメーカーと名前" }
-            ]},
-]}
+$servername = "localhost"; // データベースのホスト名
+$username = "ユーザー名"; // データベースのユーザー名
+$password = "パスワード"; // データベースのパスワード
+$dbname = "データベース名"; // 使用するデータベース名
+
+/ データベースに接続する
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 接続エラーのチェック
+if ($conn->connect_error) {
+    die("接続エラー: " . $conn->connect_error);
+}
+
+// ペットの名前を取得するSQLクエリ
+$sql = "SELECT * FROM pets";
+
+// クエリを実行して結果を取得
+$result = $conn->query($sql);
+
+// 結果を処理する（例：取得したペット名の表示）
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ペット名: " . $row["pet_name"] . "<br>";
+    }
+} else {
+    echo "データがありません";
+}
