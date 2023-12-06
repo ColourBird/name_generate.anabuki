e3a81bb94366fed96fa383cdb60fb446581e8a20
ColourBird <130944207+ColourBird@users.noreply.github.com>
2023-12-06 11:14:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-
+{ 
+ "users":[
+            {
+             "name":"ユーザー1",
+             "pass":"パスワード1",
+             "favorites": {
+                "pets":["タマ", "ポチ","ミケ"],
+                "people": [
+                  { "family": "佐藤", "last": "太郎" },
+                  { "family": "鈴木", "last": "花子" }
+                ],
+                "cars": [
+                  { "company": "トヨタ", "car": "カローラ" },
+                  { "company": "日産", "car": "ノート" }
+                ]},
+             "history":[
+                { "type": "pets", "name": "タマ", "concept": "ペット名" },
+                { "type": "people", "name": "佐藤太郎", "concept": "日本人の苗字と名前" },
+                { "type": "cars", "name": "トヨタカローラ", "concept": "車のメーカーと名前" }
+            ]},
+]}
