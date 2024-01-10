984187b77f07e97114ad82a2b32ace9e2962ec34
バハムートbkb <153044136+bahabkb@users.noreply.github.com>
2024-01-10 11:09:25 +0900

    Create name_log.php

---
+++ name_log.php
@@ -0,0 +1,17 @@
+<?php
+# DB接続してデータを取得する
+try {
+    $db = new PDO("mysql:dbname=mydb2;host=127.0.0.1;charset=utf8", "root", "");
+} catch (PDOException $e) {
+    echo "DB接続エラー" . $e->getMessage();
+    die();
+}
+
+# クエリを実行してデータを取得
+$result = $db->query("SELECT * FROM favorites");
+
+# 結果を取得して表示
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<p>" . $row['history'] . "</p>"; 
+}
+?>
